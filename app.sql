e50d465ac07e5d2616c17f4c21b5ed555355efd6
Monicaptrn <130291220+Monicaptrn@users.noreply.github.com>
2023-12-03 09:58:35 +0000

    UPDATE

--- app.sql
+++ app.sql
@@ -1,26 +1,28 @@
-drop table if exists schedule;
-create table schedule (
-	id serial,
-	doctor_name text,
-	patient_name text,
+drop table if exists employee;
+create table employee (
+	id_employee text,
+	employee_name text,
 	gender text,
-	symptom text,
+	date_of_birth date,
+	position text,
 	handphone text,
 	address text,
-	waktu time,
-	tanggal date
+	start_and_finish_time time,
+	total_working_hours time,
+	salary text
 );
 
-insert into schedule (doctor_name, patient_name, gender, symptom, handphone, address, waktu, tanggal) 
+insert into employee (id_employee, employee_name, gender, date_of_birth, position, handphone, address, start_and_finish_time, total_working_hours, salary) 
 values
-	('dr. Nurita', 'Ahmad Maulana', 'male', '["headache", "stomache"]', 62838, 'address1', '08:00', '2023-10-01'),
-	('dr. Yogi', 'Renata Zahab', 'female', '["cough", "flu"]', 62838, 'address2', '09:00', '2022-10-02'),
-	('dr. Wibowo', 'Nunuk Reni', 'female', '["cough", "flu"]', 62838, 'address3', '10:00', '2022-10-03'),
-	('dr. Yogi', 'Bro Ulil', 'male', '["cough", "headache"]', 62838, 'address4', '11:00', '2022-10-04'),
-	('dr. Ulama', 'Wah Bowi', 'male', '["headache", "flu"]', 62838, 'address5', '12:00', '2022-10-05'),
-	('dr. Ulama', 'Iis Mika', 'female', '["cough", "flu", "stomache", "headache"]', 62838, 'address6', '08:00', '2022-10-06'),
-	('dr. Ping', 'Zizah Lana', 'female', '["flu", "stomache", "headache"]', 62838, 'address7', '09:00', '2022-10-07'),
-	('dr. Nurita', 'Alif Iman', 'male', '["cough", "flu", "headache"]', 62838, 'address8', '10:00', '2022-10-08'),
-	('dr. Ping', 'Zaka Zaki', 'female', '["cough", "stomache", "headache"]', 62838, 'address9', '11:00', '2022-10-09'),
-	('dr. Wibowo', 'Faus Rahmi', 'male', '["cough"]', 62838, 'address10', '12:00', '2022-10-11')
+	('1015', 'Givangkara Ozora Ilhamsah', 'Male', '2023-10-01', 'Waiter', 6285780, '["09:00", "17:00"]', '08:00', 'Rp400.000'),
+	('1014', 'Naia Diti Sabrina Aurelia','Female', '2023-10-01', 'Waiter', 6285779, '["06:00", "17:00"]', '08:00', 'Rp550,000'),
+	('1013', 'Tatia Widya Ningrum', 'Female', '2023-10-01', 'Waiter', 6285778, '["06:00", "17:00"]', '11:00', 'Rp550,000'),
+	('1012', 'Ananda Masayu Firda', 'Female', '2023-10-01', 'Waiter', 6285777,	'["07:00", "17:00"]', '10:00', 'Rp500,000'),
+	('1011', 'M Aqullah Dana', 'Male', '2023-10-01', 'Waiter', 6285776, '["09:00", "17:00"]', '08:00', 'Rp400,000'),
+	('1010', 'Vina Alfita Sari', 'Female', '2023-10-01', 'Waiter', 6285775, '["08:00", "17:00"]', '09:00', 'Rp450,000'),
+	('1009', 'Farhan Rasyid Ramadhan', 'Male', '2023-10-01', 'Waiter', 6285774, '["07:00", "17:00"]', '10:00', 'Rp500,000'),
+	('1004', 'Jorell Ramos Sinaga', 'Male', '2023-10-01', 'Cashier', 6285769, '["06:00", "17:00"]', '11:00', 'Rp550,000'),
+	('1003', 'Ara Dina Mumtaza', 'Female', '2023-10-01', 'Cashier', 6285768, '["07:00", "17:00"]', '10:00', 'Rp500,000'),
+	('1002', 'Azzahra Ayungga Sukma', 'Female', '2023-10-01', 'Cheff', 6285767, '["06:00", "17:00"]', '11:00', 'Rp550,000'),
+	('1001', 'Aisyah Zayyan Putri', 'Female', '2023-10-01', 'Cheff', 6285766, '["06:00", "17:00"]', '11:00', 'Rp550,000')
 	;
